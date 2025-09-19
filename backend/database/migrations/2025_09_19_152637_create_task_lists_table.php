f16e78d67d5641a0a2161d772799d3cfd5c014ff
JamiuKolawole04 <jakko4real@gmail.com>
2025-09-19 16:45:59 +0100

    chore: updating migrations

--- backend/database/migrations/2025_09_19_152637_create_task_lists_table.php
+++ backend/database/migrations/2025_09_19_152637_create_task_lists_table.php
@@ -12,7 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('task_lists', function (Blueprint $table) {
-            $table->id();
+           $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->foreignId('task_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->boolean('completed')->default(false);
             $table->timestamps();
         });
     }
